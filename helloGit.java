eb63382c78a8301d2b950287f88f3f0232ad4806
wy <1033522838@qq.com>
2020-07-10 11:15:10 +0800

    SSM第二次修改

--- helloGit.java
+++ helloGit.java
@@ -1,6 +1,7 @@
-public class HelloGit{
+﻿public class HelloGit{
 	public static void main(String[]args){
               System.out.println("SSM,进行了修改");
-	}
+  System.out.println("SSM,进行了第二次修改");
+ 	}
 }
 
